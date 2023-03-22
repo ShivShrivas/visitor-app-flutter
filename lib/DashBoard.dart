38a2e423eb4899aafd1a9770f22eb807cc30c89e
admin <er.amitsharma957@gmail.com>
2023-03-22 13:16:15 +0530

    all static things done

--- lib/DashBoard.dart
+++ lib/DashBoard.dart
@@ -12,17 +12,35 @@ import 'gridDashboard.dart';
 class Dashboard extends StatefulWidget {
   final LoginResponse loginResponse;
 
-  const Dashboard({Key? key, required this.loginResponse, required LoginResponse LoginResponse}) : super(key: key);
+  const Dashboard(
+      {Key? key,
+      required this.loginResponse,
+      required LoginResponse LoginResponse})
+      : super(key: key);
 
   @override
   State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  late List<PurposeResponse> _currencies=<PurposeResponse>[];
+   late List<PurposeResponse> currencies;
+
+
+   PurposeResponse? selectedPurpose;
+
+TextEditingController timeController=TextEditingController();
+TextEditingController dateController=TextEditingController();
+
+
+
 
   Future<void> getPurposeResponse() async {
-    Map<String, dynamic> body = {"Action":5,"FYId":1,"SessionId":1,"RelationshipId":1};
+    Map<String, dynamic> body = {
+      "Action": 5,
+      "FYId": 1,
+      "SessionId": 1,
+      "RelationshipId": 1
+    };
     String jsonBody = json.encode(body);
     final encoding = Encoding.getByName('utf-8');
     final url = Uri.parse(
@@ -38,145 +56,150 @@ class _DashboardState extends State<Dashboard> {
     final List<dynamic> parsed = json.decode(response.body);
     print(response.body);
 
+    setState(() {
+      currencies = (json.decode(response.body) as List)
+          .map((i) => PurposeResponse.fromJson(i))
+          .toList();
+        selectedPurpose=currencies[0];
+    });
+    print(currencies[0].purposeName);
+  }
 
-   setState(() {
-     _currencies=(json.decode(response.body) as List).map((i) =>
-         PurposeResponse.fromJson(i)).toList();
-   });
-    print(_currencies[0].purposeName);
+  @override
+  void initState() {
+    setState(() {
+      getPurposeResponse();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
-
-          Padding(padding: EdgeInsets.all(0),
-          child:   Container(
-
-            alignment: Alignment.topCenter,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/background.png'),
-                    fit: BoxFit.fill
-                )
-            ),
-            height: 270,
-            child: SizedBox(
-
-              height: 230,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 10, right: 10),
-
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Container(child:
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: GestureDetector(
-                                  onTap: () {
-
-                                  },
-                                  child: Container(
-                                    clipBehavior: Clip.antiAlias,
-                                    // child:  Image.network('https://picsum.photos/250?image=9'),
-                                    width: 60.0,
-                                    height: 60.0,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        image: DecorationImage(
-                                            image: AssetImage('assets/images/img.png'),
-                                            fit: BoxFit.cover),
-                                        borderRadius:
-                                        BorderRadius.all(Radius.circular(75.0)),
-                                        boxShadow: [
-                                          BoxShadow(blurRadius: 2.0, color: Colors.black)
-                                        ]),
+          Padding(
+            padding: EdgeInsets.all(0),
+            child: Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/background.png'),
+                      fit: BoxFit.fill)),
+              height: 270,
+              child: SizedBox(
+                height: 230,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: GestureDetector(
+                                    onTap: () {},
+                                    child: Container(
+                                      clipBehavior: Clip.antiAlias,
+                                      // child:  Image.network('https://picsum.photos/250?image=9'),
+                                      width: 60.0,
+                                      height: 60.0,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          image: DecorationImage(
+                                              image: AssetImage(
+                                                  'assets/images/img.png'),
+                                              fit: BoxFit.cover),
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(75.0)),
+                                          boxShadow: [
+                                            BoxShadow(
+                                                blurRadius: 2.0,
+                                                color: Colors.black)
+                                          ]),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Container(
-                                    width: 230,
-                                    child: Text(
-                                      widget.loginResponse.userName,
-                                      style: GoogleFonts.openSans(
-                                        textStyle: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Container(
+                                      width: 210,
+                                      child: Text(
+                                        widget.loginResponse.userName,
+                                        style: GoogleFonts.openSans(
+                                          textStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                        ),
                                       ),
                                     ),
-                                  ),
-
-                                  SizedBox(height: 4),
-                                  Text(
-                                    widget.loginResponse.schoolAddress,
-                                    style: GoogleFonts.openSans(
-                                      textStyle: TextStyle(
-                                          color: Colors.white38,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      widget.loginResponse.schoolAddress,
+                                      style: GoogleFonts.openSans(
+                                        textStyle: TextStyle(
+                                            color: Colors.white38,
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.w600),
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),)
-                        ,
-                        IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => NotificationPage(),
-                                ));
-                          },
-                          alignment: Alignment.centerLeft,
-                          icon: Image.asset("assets/images/notification.png", width: 24),
-                        )
-                      ],
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => NotificationPage(),
+                                  ));
+                            },
+                            alignment: Alignment.centerLeft,
+                            icon: Image.asset("assets/images/notification.png",
+                                width: 24),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  GestureDetector(
-                    child: Container(),
-                  ),
-                  
-
-                ],
+                    GestureDetector(
+                      child: Container(),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ), ),
+          ),
 
-
-
-          SizedBox(height:60,
-          child: Text(
-
-            "Choose an Operation",style: TextStyle(
-fontSize: 20,color: Colors.indigo
-          ),)),
+          SizedBox(
+              height: 60,
+              child: Text(
+                "Choose an Operation",
+                style: TextStyle(fontSize: 20, color: Colors.indigo),
+              )),
 
           //TODO Grid Dashboard
           GridDashboard()
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add, color: Colors.white,),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
         backgroundColor: Colors.indigoAccent,
-        onPressed: (){
-          getPurposeResponse();
+        onPressed: () {
+
           showDialog(
               context: context,
               builder: (BuildContext context) {
@@ -203,61 +226,104 @@ fontSize: 20,color: Colors.indigo
                           TextFormField(
                             decoration: InputDecoration(
                               labelText: 'Mobile',
-                              icon: Icon(Icons.call ),
-                            ),
-                          ), TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Meeting with',
-                              icon: Icon(Icons.people ),
+                              icon: Icon(Icons.call),
                             ),
                           ),
-
-                         Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                           children: [
-                             Icon(Icons.speaker_notes,color: Colors.grey,),
-
-                             DropdownButton(
-
-                               // hint: Text('Choose Company'), // Not necessary for Option 1
-                               onChanged: (newValue) {
-                                 setState(() {
-                                   print(_currencies);
-                                   // here i have taken the boolen variable to show and hide the list if you have not seleted the value from the dropdown the it will show the text and if selected the it will show you the list.
-                                 });
-                                 print(_currencies);
-                               },
-                               items: _currencies.map((company) {
-                                 return DropdownMenuItem(
-                                   child: new Text(company.purposeName.toString()),
-                                   value: company.code,
-                                 );
-                               }).toList(),
-                             ),
-                           ],
-                         ),
-
                           TextFormField(
                             decoration: InputDecoration(
-                              labelText: 'Time',
-                              icon: Icon(Icons.watch_later ),
+                              labelText: 'Meeting with',
+                              icon: Icon(Icons.people),
                             ),
-                          ), TextFormField(
+                          ),
+                          Row(
+
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(
+                                Icons.speaker_notes,
+                                color: Colors.grey,
+                              ),
+                              Container(
+                                height: 60,
+                                width: 200,
+                                child: DropdownButton<PurposeResponse>(
+                                  alignment: Alignment.center,
+
+                                  value: selectedPurpose,
+
+
+                                  onChanged: (PurposeResponse? data) {
+                                      print(data?.purposeName.toString());
+                                      selectedPurpose = data!;
+                                      // _chapterCode = selectChapterCode!.Code;
+
+                                  },
+                                  items: currencies.map((PurposeResponse data) {
+                                    return DropdownMenuItem<PurposeResponse>(
+                                      child: Text("  " + data.purposeName.toString(),
+                                          style: const TextStyle(
+                                              fontSize: 12.0,
+                                              fontFamily: 'Montserrat',
+                                              fontWeight: FontWeight.w700)),
+                                      value: data,
+                                    );
+                                  }).toList(),
+
+                                  hint: const Text(
+                                    "Select Purpose",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              )
+
+                            ],
+                          ),
+                          TextFormField(
+                          controller: timeController,
+
                             onTap: () async {
                               print("done");
                               TimeOfDay? time = await getTime(
                                 context: context,
                                 title: "Select Your Time",
+
                               );
+                              final localizations = MaterialLocalizations.of(context);
+                              final formattedTimeOfDay = localizations.formatTimeOfDay(time!);
+                              timeController.text=formattedTimeOfDay;
                               print(time);
 
                             },
                             decoration: InputDecoration(
+                              labelText: 'Time',
+                              icon: Icon(Icons.watch_later),
+                            ),
+
+                          ),
+                          TextFormField(
+                            controller: dateController,
+                            onTap: () async{
+                              DateTime? date = DateTime(1900);
+                              FocusScope.of(context).requestFocus(FocusNode());
+                              date = await showDatePicker(
+                                  context: context,
+                                  initialDate:DateTime.now(),
+                                  firstDate:DateTime(1900),
+                                  lastDate: DateTime(2100));
+
+                              dateController.text = "${date?.toLocal()}".split(' ')[0];
+
+
+                              },
+                            decoration: InputDecoration(
+
                               labelText: 'Date',
-                              icon: Icon(Icons.calendar_month ),
+                              icon: Icon(Icons.calendar_month),
                             ),
                           ),
-
                         ],
                       ),
                     ),
@@ -300,5 +366,4 @@ fontSize: 20,color: Colors.indigo
 
     return time;
   }
-
 }
